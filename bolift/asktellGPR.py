4b061ce98810b1c37a2115a2a4956f0a84390758
maykcaldas <maykcaldas@gmail.com>
2023-03-09 18:11:10 -0500

    update experiments

--- bolift/asktellGPR.py
+++ bolift/asktellGPR.py
@@ -96,16 +96,16 @@ class AskTellGPR(AskTellFewShotTopk):
     def _predict(self, X):
         if(len(X) == 0):
             raise ValueError("X is empty")
-        embedding=self._query_cache(X)
-        # embedding = np.array(self._embedding.embed_documents(X, 250))
+        # embedding=self._query_cache(X)
+        embedding = np.array(self._embedding.embed_documents(X, 250))
         results = []
         means, stds = self.regressor.predict(embedding, return_std=True)
         results = [GaussDist(mean, std) for mean, std in zip(means, stds)]
         return results, 0
 
     def _train(self, X, y):
-        embedding=self._query_cache(X)
-        # embedding = np.array(self._embedding.embed_documents(X, 250))
+        # embedding=self._query_cache(X)
+        embedding = np.array(self._embedding.embed_documents(X, 250))
         self.regressor.fit(embedding, list(map(float, y)))
 
 
@@ -145,3 +145,42 @@ class AskTellGPR(AskTellFewShotTopk):
             ], 
             [ex["y"] for ex in self.examples]
         )
+
+    def predict(self, x: str) -> Union[Tuple[float, float], List[Tuple[float, float]]]:
+        """Predict the probability distribution and values for a given x.
+
+        Args:
+            x: The x value(s) to predict.
+        Returns:
+            The probability distribution and values for the given x.
+
+        """
+        if not isinstance(x, list):
+            x = [x]
+        if not self._ready:
+            # special zero-shot
+            self.prompt = self._setup_prompt(
+                None, self._prompt_template, self._suffix, self._prefix
+            )
+            self.inv_prompt = self._setup_inverse_prompt(None)
+            self.llm = self._setup_llm(self._model)
+            self._ready = True
+
+        if self._selector_k is not None:
+            # have to update this until my PR is merged
+            self.prompt.example_selector.k = min(self._example_count, self._selector_k)
+
+        queries = [
+            self.prompt.format(
+                x=self.format_x(x_i),
+                y_name=self._y_name,
+            )
+            for x_i in x
+        ]
+        results, tokens = self._predict(queries)
+        self.tokens_used += tokens
+
+        # compute mean and standard deviation
+        if len(x) == 1:
+            return results[0]
+        return results
