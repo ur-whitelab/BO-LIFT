f2e7ae2e9db25a47cccd27566120b69954879786
maykcaldas <maykcaldas@gmail.com>
2023-03-27 21:07:03 -0400

    implemented KRR and KNN

--- bolift/__init__.py
+++ bolift/__init__.py
@@ -2,4 +2,6 @@ from .version import __version__
 from .asktell import AskTellFewShotMulti, AskTellFewShotTopk
 from .asktellfinetuning import AskTellFinetuning
 from .asktellGPR import AskTellGPR
+from .asktellRidgeRegression import AskTellRidgeKernelRegression
+from .asktellNearestNeighbor import AskTellNearestNeighbor
 from .pool import Pool
