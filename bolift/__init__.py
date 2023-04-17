6e8fe6056c11c18b5ba3b581669acf550cdcba3a
Andrew White <white.d.andrew@gmail.com>
2023-04-17 00:26:24 -0700

    Fixed tool name in __init__.py

--- bolift/__init__.py
+++ bolift/__init__.py
@@ -9,4 +9,4 @@ except ImportError:
 from .asktellRidgeRegression import AskTellRidgeKernelRegression
 from .asktellNearestNeighbor import AskTellNearestNeighbor
 from .pool import Pool
-from .tool import BOLiftTools
+from .tool import BOLiftTool
