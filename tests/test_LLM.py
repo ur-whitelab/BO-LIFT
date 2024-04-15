bb7b406178e313b4d1be550db8110e247f81962e
maykcaldas <maykcaldas@gmail.com>
2024-04-15 12:44:26 -0400

    Started implementation of LLM-specific tests

--- tests/test_LLM.py
+++ tests/test_LLM.py
@@ -25,6 +25,17 @@ class TestLLM(ABC):
         result, token = llm.predict("The value of 1 + 1 is")
         assert result[0].mean() == 2
 
+class TestChatOpenAILLM(TestLLM):
+    __test__ = True
+
+    @classmethod
+    def models_to_test(cls):
+        return ["gpt-3.5-turbo"]
+    
+    def test_parse(self, model_name):
+        llm = llm_model.get_llm(model_name=model_name, stop=["\n"])
+        result, token = llm.predict("The value of 1 + 1 is")
+        assert result[0].mean() == 2
 
 class TestOpenAILLM(TestLLM):
     __test__ = True
@@ -32,36 +43,4 @@ class TestOpenAILLM(TestLLM):
     @classmethod
     def models_to_test(cls):
         return ["gpt-3.5-turbo-instruct"]
-
-
-class TestChatOpenAILLM(TestLLM):
-    __test__ = True
-
-    @classmethod
-    def models_to_test(cls):
-        return ["gpt-3.5-turbo-0125"]
-    
-    def test_parse_response(self, model_name):
-        print(model_name)
-        prompt = "2 + 2 is"
-        answer = 4
-        llm = llm_model.get_llm(
-            n=3,
-            best_of=3,
-            temperature=1,
-            model_name=model_name,
-            top_p=0.99,
-            stop=["\n"],
-            logprobs=1,
-        )
-
-        query = [
-            SystemMessage(content=""),
-            HumanMessage(content=prompt)
-        ]
-
-        g = llm.llm.generate([query]).generations
-        result = llm.parse_response(g[0])
-
-        assert abs(result.mode().astype(int) - answer) <= 1
 
