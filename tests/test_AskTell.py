47928cf4b27dfcf532c177dcb30d648ba5309877
maykcaldas <maykcaldas@gmail.com>
2024-06-11 10:49:06 -0400

    fixed conflicts after merging the update_tests branch

--- tests/test_AskTell.py
+++ tests/test_AskTell.py
@@ -3,7 +3,7 @@ from bolift import (AskTellFewShotMulti,
                     AskTellFewShotTopk,
                     AskTellGPR,
                     AskTellNearestNeighbor,
-                    # AskTellRidgeKernelRegression,
+                    AskTellRidgeKernelRegression,
                     AskTellFinetuning)
 import numpy as np
 from abc import ABC
@@ -17,91 +17,62 @@ np.random.seed(0)
 
 def pytest_generate_tests(metafunc):
     if 'asktell_class' in metafunc.fixturenames:
-        models = metafunc.cls.asktells_to_test()
-        metafunc.parametrize("asktell_class", models)
-    if 'model_name' in metafunc.fixturenames:
         models = metafunc.cls.models_to_test()
-        metafunc.parametrize("model_name", models)
-
+        metafunc.parametrize("asktell_class", models)
 
 class TestAskTell(ABC):
-    __test__ = False # Will only test in the children classes
+    __test__ = False
 
-    def test_tell(self, asktell_class):
-        asktell = asktell_class(x_formatter=lambda x: f"y = {x}+2")
-        for k in range(5):
-            asktell.tell(k, k+2)
-        assert asktell._example_count == 5
-
-    def test_fewshot(self, asktell_class, model_name):
-        asktell = asktell_class(model=model_name,
-                                x_formatter=lambda x: f"y = 2*{x}"
-                                )
+    def test_fewshot(self, asktell_class):
+        asktell = asktell_class(x_formatter=lambda x: f"y = 2*{x}")
         asktell.tell(1, 2)
         asktell.tell(2, 4)
         asktell.tell(4, 8)
         asktell.tell(16, 32)
 
-        sys_msg = "You are a calculator. Given the prompt, provide the answer. Please complete the result only without any explanation."
-
-        dist = asktell.predict(2, system_message=sys_msg)
+        dist = asktell.predict(2)
         m = dist.mode()
         assert m == pytest.approx(4, 0.001)
         
-        dist = asktell.predict(3, system_message=sys_msg)
+        dist = asktell.predict(3)
         m = dist.mode()
         assert m == pytest.approx(6, 0.001)
 
-    def test_inverse_fewshot(self, asktell_class, model_name):
-
-        sys_msg = "You are a inverse calculator. Given the number in the prompt, your task is to provide a matemathical equation that generates that number. Please complete the mathematical equation only without any explanation."
-
-        asktell = asktell_class(model=model_name,
-                                x_formatter=lambda x: f"y = 2*{x}"
-                                )
+    def test_inv_fewshot(self, asktell_class):
+        asktell = asktell_class(x_formatter=lambda x: f"2*{x}")
         asktell.tell(1, 2)
         asktell.tell(2, 4)
         asktell.tell(4, 8)
         asktell.tell(16, 32)
 
-        inverse = asktell.inv_predict(8, system_message=sys_msg)
+        inverse = asktell.inv_predict(8)
         assert "*" in inverse
-
 
 class TestAskTellMulti(TestAskTell):
     __test__ = False
 
-
 class TestAskTellTopK(TestAskTell):
     __test__ = True
 
-    @classmethod
-    def asktells_to_test(cls):
-        return [AskTellFewShotTopk]
+    @pytest.fixture
+    def asktell_class():
+        return AskTellFewShotTopk
     
-    @classmethod
-    def models_to_test(cls):
-        return ["gpt-3.5-turbo-instruct", "gpt-3.5-turbo"]
-    
-
 class TestAskTellKNN(TestAskTell):
     __test__ = False
-
 
 class TestAskTellKRR(TestAskTell):
     __test__ = False
 
-
 class TestAskTellGPR():
     __test__ = True
 
-    # @classmethod
-    # def asktells_to_test(cls):
-    #     return [AskTellGPR]
+    @pytest.fixture
+    def asktell_class():
+        return AskTellGPR
     
-    # @pytest.mark.parametrize("asktell_class", [AskTellGPR])
-    def test_gpr(self):
-        asktell = AskTellGPR(
+    def test_gpr(asktell_class):
+        asktell = asktell_class(
             x_formatter=lambda x: f"y = 2 + {x}",
             y_formatter=lambda y: str(int(y)),
         )
@@ -111,12 +82,11 @@ class TestAskTellGPR():
         asktell.predict(5000)
         assert asktell.ask([1, 5])[0][0] == 5
 
-
 class TestAskTellFineTuning():
-    __test__ = False
+    __test__ = True
 
     @pytest.fixture
-    def asktells_to_test():
+    def asktell_class():
         return AskTellFinetuning
     
     def test_prepare_data_finetuning(asktell_class):
