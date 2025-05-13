d7ae15882b80c15992ad1195bd91b1198e91e4c5
maykcaldas <mayk@futurehouse.org>
2025-05-13 00:34:17 -0700

    Updated package name

--- boicl/__init__.py
+++ boicl/__init__.py
@@ -6,10 +6,10 @@ try:
     from .asktellGPR import AskTellGPR
     from .asktellRidgeRegression import AskTellRidgeKernelRegression
 except ImportError:
-    print("GPR Packages not installed. Do `pip install bolift[gpr]` to install them")
+    print("GPR Packages not installed. Do `pip install boicl[gpr]` to install them")
 from .asktellNearestNeighbor import AskTellNearestNeighbor
 from .pool import Pool
-from .tool import BOLiftTool
+from .tool import BOICLTool
 
 
 __all__ = [
@@ -19,5 +19,5 @@ __all__ = [
     "AskTellRidgeKernelRegression",
     "AskTellNearestNeighbor",
     "Pool",
-    "BOLiftTool",
+    "BOICLTool",
 ]
